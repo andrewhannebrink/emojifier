e9ddd822efefad46c50344e26829c9d5ea60f985
andrewhannebrink <ahannebrink@safelease.com>
2022-11-09 03:13:09 -0600

    quadrant_dirs being passed

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,57 @@ use image::DynamicImage;
 use std::fs;
 
 fn main() {
-    let target_img_name = String::from("input/a/0.jpeg");
-    let parent_img_name = String::from("input/b/0.jpeg");
-
-    populate_lil_imgs_dir(img_from_path(parent_img_name));
-    render_and_save_mosaic(img_from_path(target_img_name));
+    render_from_quadrant_a_frame();
 }
 
-fn populate_lil_imgs_dir(parent_img: DynamicImage) {
+fn render_from_quadrant_a_frame () {
+    render_still_from_quadrant_frame(String::from("a"));
+}
+fn render_from_quadrant_b_frame () {
+    render_still_from_quadrant_frame(String::from("b"));
+}
+
+fn render_still_from_quadrant_frame(target_quadrant_dir: String) {
+    let mut parent_quadrant_dir = String::new();
+    if target_quadrant_dir == "a" {
+        parent_quadrant_dir = String::from("b");
+    } else {
+        parent_quadrant_dir = String::from("a");
+    }
+        
+    let target_img_name = [
+        String::from("input"),
+        target_quadrant_dir.clone(),
+        String::from("0.jpeg")
+    ].join("/");
+    let parent_img_name = [
+        String::from("input"),
+        parent_quadrant_dir.clone(),
+        String::from("0.jpeg")
+    ].join("/");
+
+    populate_lil_imgs_dir(
+        img_from_path(parent_img_name),
+        parent_quadrant_dir.clone());
+    render_and_save_mosaic(img_from_path(target_img_name));
+
+}
+
+fn populate_lil_imgs_dir(parent_img: DynamicImage, parent_quadrant_dir: String) {
 
     fs::remove_dir_all([
         String::from("io/lil_imgs"), 
-        String::from("b")
+        parent_quadrant_dir.clone()
     ].join("/"));
     fs::create_dir([
         String::from("io/lil_imgs"), 
-        String::from("b")
+        parent_quadrant_dir.clone()
     ].join("/"));
 
     compose_mosaic_from_paths(
         parent_img, 
         true, 
-        String::from("b"));
+        parent_quadrant_dir.clone());
 }
 
 fn render_and_save_mosaic(target_img: DynamicImage) {
@@ -46,7 +75,7 @@ fn compose_mosaic_from_paths(
         quadrant_dir: String) {
 
 
-    let depth = 32;
+    let depth = 18;
     let (xt, yt) = (1920, 1080);
     let crop_details = mosaic::CropDetails {
         depth: depth,
