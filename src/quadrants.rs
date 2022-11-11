91e80c0f4dd0244bc9e354b20beb1fb0e9b775e3
andrewhannebrink <ahannebrink@safelease.com>
2022-11-11 12:00:50 -0600

    quadrants_dir seems to be working

--- src/quadrants.rs
+++ src/quadrants.rs
@@ -4,18 +4,29 @@ use image::imageops::resize;
 use image::imageops::replace;
 
 pub fn frames_into_quadrants() {
-    for i in 0..5 { 
+    let total_frames = 4;
+    for i in 1..total_frames + 1 { 
         compose_one_quadrants_frame(i);
     }
 }
 
 fn compose_one_quadrants_frame(frame_int: i32) {
     let frame_str = prepend_zeroes(frame_int as usize);
-    let mut view_port_img = image::open("io/input/a/0001.jpeg").unwrap();
-    let mut quadrant_a_img = image::open("io/input/a/0001.jpeg").unwrap();
-    let mut quadrant_b_img = image::open("io/input/b/0001.jpeg").unwrap();
-    let mut quadrant_c_img = image::open("io/output/a/0001.jpeg").unwrap();
-    let mut quadrant_d_img = image::open("io/output/b/0001.jpeg").unwrap();
+    println!("{}", frame_str);
+    let mut view_port_img = 
+        image::open(file_path(frame_str.clone(), "io/input/a")).unwrap();
+    let mut quadrant_a_img = image::open(file_path(
+            frame_str.clone(),
+            "io/input/a")).unwrap();
+    let mut quadrant_b_img = image::open(file_path(
+            frame_str.clone(),
+            "io/input/b")).unwrap();
+    let mut quadrant_c_img = image::open(file_path(
+            frame_str.clone(),
+            "io/output/a")).unwrap();
+    let mut quadrant_d_img = image::open(file_path(
+            frame_str.clone(),
+            "io/output/b")).unwrap();
     let quadrant_a_img_resized = resize(&mut quadrant_a_img,
         1920/2, 1080/2, FilterType::Nearest);
     let quadrant_b_img_resized = resize( &mut quadrant_b_img,
@@ -28,7 +39,16 @@ fn compose_one_quadrants_frame(frame_int: i32) {
     replace(&mut view_port_img, &quadrant_b_img_resized, 1920/2, 0);
     replace(&mut view_port_img, &quadrant_c_img_resized, 0, 1080/2);
     replace(&mut view_port_img, &quadrant_d_img_resized, 1920/2, 1080/2);
-    view_port_img.save("io/output/quadrants/0001.jpeg").unwrap();
+    view_port_img.save(file_path(frame_str, "io/output/quadrants")).unwrap();
+}
+
+fn file_name(frame_str: String) -> String {
+    [frame_str, ".jpeg".to_string()].concat()
+}
+
+fn file_path(frame_str: String, quadrant_src_dir: &str) -> String {
+    let name = file_name(frame_str);
+    [quadrant_src_dir.to_string(), name].join("/")
 }
 
 // TODO this is copy pasted from mosaic.rs. FIX THAT
