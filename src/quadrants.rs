1df6aa8ed635167b767c5141ea57366fecc670df
andrewhannebrink <ahannebrink@safelease.com>
2022-11-11 11:22:34 -0600

    begining of frames_into_quadrants() loop

--- src/quadrants.rs
+++ src/quadrants.rs
@@ -4,11 +4,14 @@ use image::imageops::resize;
 use image::imageops::replace;
 
 pub fn frames_into_quadrants() {
-    let mut view_port_img = image::open("io/input/a/0001.jpeg").unwrap();
-    compose_quadrants_frame(view_port_img);
+    for i in 0..5 { 
+        compose_one_quadrants_frame(i);
+    }
 }
 
-fn compose_quadrants_frame(mut view_port_img: DynamicImage) {
+fn compose_one_quadrants_frame(frame_int: i32) {
+    let frame_str = prepend_zeroes(frame_int as usize);
+    let mut view_port_img = image::open("io/input/a/0001.jpeg").unwrap();
     let mut quadrant_a_img = image::open("io/input/a/0001.jpeg").unwrap();
     let mut quadrant_b_img = image::open("io/input/b/0001.jpeg").unwrap();
     let mut quadrant_c_img = image::open("io/output/a/0001.jpeg").unwrap();
@@ -25,10 +28,19 @@ fn compose_quadrants_frame(mut view_port_img: DynamicImage) {
     replace(&mut view_port_img, &quadrant_b_img_resized, 1920/2, 0);
     replace(&mut view_port_img, &quadrant_c_img_resized, 0, 1080/2);
     replace(&mut view_port_img, &quadrant_d_img_resized, 1920/2, 1080/2);
-//  let frame_with_quadrant_a_cropped = view_port_img.crop_imm(
-//      0,
-//      0,
-//      1920 / 2,
-//      1080 / 2);
     view_port_img.save("io/output/quadrants/0001.jpeg").unwrap();
+}
+
+// TODO this is copy pasted from mosaic.rs. FIX THAT
+fn prepend_zeroes(i: usize) -> String {
+    let frame_number_without_zeroes: &str = &i.to_string();
+    let mut zeroes_to_prepend = "000";
+    if i >= 10 {
+        zeroes_to_prepend = "00";
+    } else if i >= 100 {
+        zeroes_to_prepend = "0";
+    } else if i >= 1000 {
+        zeroes_to_prepend = "";
+    }
+    [zeroes_to_prepend, frame_number_without_zeroes].concat()
 }
