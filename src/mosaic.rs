c0501473ac9f1eee0369d7e4adfeb37dd03a40f0
andrewhannebrink <ahannebrink@safelease.com>
2022-11-13 14:45:53 -0600

    resize running much faster

--- src/mosaic.rs
+++ src/mosaic.rs
@@ -185,8 +185,26 @@ fn write_final_img(mut args: WriteFinalImageArgs) -> MakeMosaicReturn {
                 (x*args.c.depth + args.c.x_buf) as i64, 
                 (y*args.c.depth + args.c.y_buf) as i64
             );
-            replace(&mut final_img, &args.lil_imgs[index_in_lil_imgs as usize].img, 
-                    target_coords.0, target_coords.1);
+
+            // resize the lil_img if the dimensions are not already correct. This is here
+            // for when lil_imgs_dir is passed to make_mosaic, and the img size is not 
+            // necessarily the depth, as would be the case when lil_imgs_dir is ot passed
+            let mut resized_img: Option<DynamicImage> = Option::None;
+            if args.lil_imgs[index_in_lil_imgs as usize].img.dimensions().0 != args.c.depth {
+                resized_img = Some(args.lil_imgs[index_in_lil_imgs as usize].img.resize(
+                    args.c.depth, args.c.depth, FilterType::Gaussian));
+            }
+
+            match resized_img {
+                None => {
+                    replace(&mut final_img, &args.lil_imgs[index_in_lil_imgs as usize].img, 
+                            target_coords.0, target_coords.1);
+                },
+                Some(resized) => {
+                    replace(&mut final_img, &resized, 
+                            target_coords.0, target_coords.1);
+                }
+            }
             // TODO update lil_imgs target_coords here
             args.lil_imgs[index_in_lil_imgs as usize].target_coords.push(target_coords);
             //dbg!("{:?}", args.lil_imgs[index_in_lil_imgs as usize].target_coords);
@@ -347,11 +365,11 @@ fn get_lil_imgs_from_dir(
         let mut img = image::open(img_path).unwrap();
 
         // TODO only resize if the image is actually used and the dimensions are different
-        let resized_img = img.resize(
-            crop_details.depth, crop_details.depth, FilterType::Gaussian);
+        //let resized_img = img.resize(
+        //    crop_details.depth, crop_details.depth, FilterType::Gaussian);
         lil_imgs.push(ImageInfo {
             avg_color: get_avg_rgb(&img, skip as u8),
-            img: resized_img,
+            img,
             parent_coords: (0, 0),
             target_coords: Vec::new() // TODO parent_coords are not relevant for getting from dir
         });
