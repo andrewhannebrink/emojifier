d564f05d853f9492180d64718449ce2198f825fd
andrewhannebrink <ahannebrink@safelease.com>
2022-11-13 14:01:29 -0600

    big emoji dir in py script

--- scripts/lil_img_ops.py
+++ scripts/lil_img_ops.py
@@ -7,7 +7,15 @@ def whiteBackgrounds():
         #print(imageName)
         os.system(f'convert io/lil_imgs/emoji_raw/{imageName} -resize 60x60^ -background white -alpha remove -alpha off io/lil_imgs/emoji/{imageName}')
 
+def bigImages():
+    allImageNames = os.listdir("io/lil_imgs/emoji_raw")
+    os.system('rm -rf io/lil_imgs/emoji_big && mkdir io/lil_imgs/emoji_big')
+    for imageName in allImageNames:
+        #print(imageName)
+        os.system(f'convert io/lil_imgs/emoji_raw/{imageName} -resize 200x200^ -background white -alpha remove -alpha off io/lil_imgs/emoji_big/{imageName}')
+
 def main():
-    whiteBackgrounds()
+    #whiteBackgrounds()
+    bigImages()
 
 main()
