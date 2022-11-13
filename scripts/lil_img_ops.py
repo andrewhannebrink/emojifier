360b3d377ebb3fd834842c382f4271a42871fcd4
andrewhannebrink <ahannebrink@safelease.com>
2022-11-13 15:18:12 -0600

    emoji buffered border ops

--- scripts/lil_img_ops.py
+++ scripts/lil_img_ops.py
@@ -14,8 +14,24 @@ def bigImages():
         #print(imageName)
         os.system(f'convert io/lil_imgs/emoji_raw/{imageName} -resize 200x200^ -background white -alpha remove -alpha off io/lil_imgs/emoji_big/{imageName}')
 
+def trimWhiteSpace():
+    allImageNames = os.listdir("io/lil_imgs/emoji")
+    os.system('rm -rf io/lil_imgs/emoji_trim && mkdir io/lil_imgs/emoji_trim')
+    for name in allImageNames:
+        os.system(f'convert io/lil_imgs/emoji/{name} -trim io/lil_imgs/emoji_trim/{name}')
+
+
+def centerImagesOnWhiteTile(): 
+    allImageNames = os.listdir("io/lil_imgs/emoji_trim")
+    os.system('rm -rf io/lil_imgs/emoji_buffered && mkdir io/lil_imgs/emoji_buffered')
+    for name in allImageNames:
+        os.system(f'convert -size 50x50 xc:white io/lil_imgs/emoji_trim/{name} -gravity center -composite io/lil_imgs/emoji_buffered/{name}')
+
+
 def main():
     #whiteBackgrounds()
-    bigImages()
+    #bigImages()
+    #trimWhiteSpace()
+    centerImagesOnWhiteTile()
 
 main()
