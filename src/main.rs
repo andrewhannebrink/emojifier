e876e070ead5e0932f10514fef13a092dcf95449
andrewhannebrink <ahannebrink@safelease.com>
2022-11-20 12:01:54 -0600

    removed a/b lil_img dirs in git

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use std::time::Instant;
 fn main() {
     let now = Instant::now();
     let instructions = instruct::get_instructions();
-    transpose::transpose_every_frame(&instructions);
+    transpose::transpose_every_frame(&instructions, true);
+    //transpose::transpose_every_frame(&instructions, false);
     //quadrants::frames_into_quadrants();
     let elapsed_time = now.elapsed();
     println!("main() took {} seconds.", elapsed_time.as_secs());
