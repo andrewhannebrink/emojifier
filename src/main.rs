f11834ec6c7de9c9c5092e29abf60f5f40f5ca0d
andrewhannebrink <ahannebrink@safelease.com>
2022-11-25 00:46:53 -0600

    sdg vid full 34 seconds finishes in under one min in release mode :-)

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ fn main() {
     let n = path::prepend_zeroes(1452);
     println!("{}", n);
     let instructions = instruct::get_instructions();
-    //transpose::transpose_every_frame(&instructions, true);
-    transpose::transpose_every_frame(&instructions, false);
-    quadrants::frames_into_quadrants();
+    transpose::transpose_every_frame(&instructions, true);
+    //transpose::transpose_every_frame(&instructions, false);
+    //quadrants::frames_into_quadrants();
     let elapsed_time = now.elapsed();
     println!("main() took {} seconds.", elapsed_time.as_secs());
 }
