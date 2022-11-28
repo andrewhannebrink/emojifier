f2a6c6d0c36c8c28bbd0185cd21dbd18899a04c7
andrewhannebrink <ahannebrink@safelease.com>
2022-11-27 18:40:35 -0600

    center zoom working

--- src/zoom.rs
+++ src/zoom.rs
@@ -10,8 +10,8 @@ static DIMENSIONS: (u32, u32) = (1920, 1080);
 #[derive(Debug)]
 struct ZoomImageInfo {
   img: DynamicImage,
-  zoom_coords: (i32, i32),
-  depth: u32
+  zoom_coords: (f32, f32),
+  depth: f32
 }
 
 fn wipe_zoom_dir() {
@@ -64,8 +64,8 @@ fn all_lil_imgs_img(lil_imgs_dir: &str) -> Vec<ZoomImageInfo>{
             }
             zoom_imgs.push(ZoomImageInfo {
                 img: lil_imgs[i].img.clone(), //TODO satisfying the borrow checker here is hard
-                zoom_coords: ((x*(sx as i64)) as i32, (y*(sy as i64)) as i32),
-                depth: sx as u32
+                zoom_coords: (x as f32 * sx, y as f32 * sy),
+                depth: sx
             });
             let temp_img = zoom_imgs[i].img.resize(
                 sx as u32, sy as u32, FilterType::Gaussian);
@@ -81,31 +81,43 @@ fn all_lil_imgs_img(lil_imgs_dir: &str) -> Vec<ZoomImageInfo>{
 }
 
 pub fn zoom(lil_imgs_dir: &str) {
-    let mut zoom_imgs = all_lil_imgs_img(lil_imgs_dir);
     let mut canvas_img: RgbaImage = plain_white_img();
-    let (z, b, d) = (4.0, 0_f32, 0_f32);
+    let mut zoom_imgs = all_lil_imgs_img(lil_imgs_dir);
+    for i in 2..32 {
+        zoom_one_frame(i, &mut zoom_imgs, &mut canvas_img.clone());
+    }
+}
+
+fn zoom_one_frame(
+        frame_int: i32, zoom_imgs: &mut Vec<ZoomImageInfo>, canvas_img: &mut RgbaImage) {
+    let z = 1.2;
+    let (b, d) = (960_f32, 540_f32);
     for i in 0..zoom_imgs.len() {
-        dbg!(zoom_imgs[i].zoom_coords);
+        //dbg!(zoom_imgs[i].zoom_coords);
         let x = zoom_imgs[i].zoom_coords.0 as f32;
         let y = zoom_imgs[i].zoom_coords.1 as f32;
-        let new_x = (z * x + b) as i32;
-        let new_y = (z * y + d) as i32;
-        dbg!(new_x, new_y);
+        let new_x = z * x + (b - b*z);
+        let new_y = z * y + (d - d*z);
+        //dbg!(new_x, new_y);
         let prev_size = zoom_imgs[i].depth;
-        let new_size = (z * prev_size as f32).floor() as u32;
+        let new_size = z * prev_size;
+        let rounded_new_size = new_size.round() as u32;
         zoom_imgs[i].zoom_coords = (new_x, new_y);
         zoom_imgs[i].depth = new_size;
 
         // if x or y is out of bounds do nothing
-        if new_y >= 0 && y.floor() as u32 + new_size <= canvas_img.dimensions().1 {
-            if new_x >= 0 && x.floor() as u32 + new_size <= canvas_img.dimensions().0 {
+        if new_y >= 0_f32 && y.floor() as u32 + rounded_new_size <= canvas_img.dimensions().1 {
+            if new_x >= 0_f32 && x.floor() as u32 + rounded_new_size <= canvas_img.dimensions().0 {
+                let new_size_int = new_size.round() as u32;
                 let temp_img = zoom_imgs[i].img.resize(
-                    new_size, new_size, FilterType::Gaussian);
-                replace(&mut canvas_img, &temp_img, new_x as i64, new_y as i64);
+                    new_size_int, new_size_int, FilterType::Gaussian);
+                replace(canvas_img, &temp_img, new_x as i64, new_y as i64);
             }
         }
     }
     //TODO test for commit
-    canvas_img.save("io/output/zoom/00002.jpeg").unwrap();
+    let frame_number_str = path::prepend_zeroes(frame_int);
+    println!("{}", frame_number_str);
+    canvas_img.save(path::zoom_output_path(&frame_number_str)).unwrap();
 }
 
