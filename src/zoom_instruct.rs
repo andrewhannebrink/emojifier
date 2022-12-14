3bfbedc0382d13d31d8bb98581ee1bfec0f3b995
Andrew Hannebrink <brink@Andrews-MacBook-Pro.local>
2022-12-14 02:14:48 -0600

    switching between zoom sequences

--- src/zoom_instruct.rs
+++ src/zoom_instruct.rs
@@ -20,16 +20,30 @@ pub struct ZoomSequence {
     pub mode: ZoomMode
 }
 
+fn full_zoom(seconds: u32) -> Vec<ZoomSequence> {
+    let mut full_zoom: Vec<ZoomSequence> = Vec::new();
+    full_zoom.push(ZoomSequence {
+        total_frames: 30 * seconds,
+        mode: ZoomMode::Zoom(ZoomInstructions { 
+            max_depth: 200, 
+            min_depth: 4, 
+            lil_imgs_dir: "io/lil_imgs/emoji_big_buffere".to_string() 
+        })
+    });
+    full_zoom
+}
+
 pub fn get_zoom_a_instructions () -> Vec<ZoomSequence> {
     let mut instructions: Vec<ZoomSequence> = Vec::new();
     instructions.push(ZoomSequence {
-        total_frames: 200,
+        total_frames: 360,
         mode: ZoomMode::Zoom(ZoomInstructions {
             max_depth: 24,
             min_depth: 4,
             lil_imgs_dir: "io/lil_imgs/emoji_big_buffered".to_string()
         })
     });
+    instructions.append(&mut full_zoom(12));
 //  instructions.push(ZoomSequence {
 //      total_frames: 200,
 //      mode: ZoomMode::Scroll(ScrollInstructions {
@@ -45,11 +59,12 @@ pub fn get_zoom_b_instructions () -> Vec<ZoomSequence> {
     instructions.push(ZoomSequence {
         total_frames: 200,
         mode: ZoomMode::Zoom(ZoomInstructions {
-            max_depth: 60,
+            max_depth: 100,
             min_depth: 20,
             lil_imgs_dir: "io/lil_imgs/emoji_big_buffered".to_string()
         })
     });
+    instructions.append(&mut full_zoom(12));
 //  instructions.push(ZoomSequence {
 //      total_frames: 200,
 //      mode: ZoomMode::Scroll(ScrollInstructions {
